e2f23d3c72fb610fff05394d5feab9e13a24cee2
Danilo Souza <danilo_souza19@outlook.com>
2020-10-02 12:06:38 -0300

    organize structure folders DDD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,12 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
-import AppError from './errors/AppError';
-import uploadConfig from './config/upload';
-
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
