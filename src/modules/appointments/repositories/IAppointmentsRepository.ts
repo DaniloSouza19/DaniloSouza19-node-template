e3f732c0ea05ea5187049b4af2668c2f5d44a989
Danilo Souza <danilo_souza19@outlook.com>
2020-12-20 11:18:05 -0300

    overwrite typeorm dependency

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import IcreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 interface IAppointmentsRepository {
+  create(data: IcreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
 
