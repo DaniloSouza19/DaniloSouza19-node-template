77014c935dd9a25cdbb07e77af66285d580a2373
Danilo Souza <danilo_souza19@outlook.com>
2021-01-01 16:26:02 -0300

    implements findByToken Method

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -18,6 +18,14 @@ class FakeUsersTokenRepository implements IUserTokensRepository {
 
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.usersTokens.find(
+      findToken => findToken.token === token
+    );
+
+    return userToken;
+  }
 }
 
 export default FakeUsersTokenRepository;
