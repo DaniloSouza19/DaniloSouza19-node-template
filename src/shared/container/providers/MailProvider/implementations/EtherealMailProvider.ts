f8dc538f317d57b80971557208cdafa679f7b82b
Danilo Souza <danilo_souza19@outlook.com>
2021-03-10 00:04:37 -0300

    refactor code

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -13,7 +13,7 @@ export default class EtherealMailProvider implements IMailProvider {
     private mailTemplateProvider: IMailTemplateProvider
   ) {
     nodemailer.createTestAccount().then(account => {
-      const transporter = nodemailer.createTransport({
+      this.transporter = nodemailer.createTransport({
         host: account.smtp.host,
         port: account.smtp.port,
         secure: account.smtp.secure,
@@ -22,8 +22,6 @@ export default class EtherealMailProvider implements IMailProvider {
           pass: account.pass,
         },
       });
-
-      this.transporter = transporter;
     });
   }
 
