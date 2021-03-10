4b01f0e4fbaac809f90676889069fd59f4b9f247
Danilo Souza <danilo_souza19@outlook.com>
2021-03-10 00:43:19 -0300

    add defaults values

--- src/config/Mail.ts
+++ src/config/Mail.ts
@@ -1,7 +1,20 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'danilo@dgstech.net',
+      name: 'Danilo da DGS Tecnologia',
+    },
+  },
 } as IMailConfig;
